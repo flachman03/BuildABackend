567138633405602b0d2d86e3267b98a3926549eb
RyanFlachman <ryan@flachman.net>
2019-08-15 09:18:50 -0600

    Create initial dataset for team names

---
+++ migrations/20190815090822_create_football_table.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('football', table => {
+    table.increments('id');
+    table.varchar('team');
+    
+  })  
+};
+
+exports.down = function(knex) {
+  
+};
