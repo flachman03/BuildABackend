2d4ec316e71a817b0395d1592f9845b5875325f4
RyanFlachman <ryan@flachman.net>
2019-08-16 09:18:59 -0600

    Add server listening on port

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ app.set('port', process.env.PORT, 3000);
 app.get('/', (req, res) => {
   res.send('oh hey')
 })
+
+app.listen(app.get('port'), () => {
+  console.log(`app is running on port on ${app.get('port')}`)
+})
