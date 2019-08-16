e55cfe7b0f3ba809e02b92d902fa1423d7580771
RyanFlachman <ryan@flachman.net>
2019-08-16 10:15:58 -0600

    Change database to the correct table of football_teams

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const app = express()
-const environment = process.env.NODE_ENV || 'development'
+const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration)
 
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-  database('football_library').select()
+  database('football_teams').select()
     .then( teams => {
-      respnse.status(200).json(teams)
+      res.status(200).json(teams)
     })
     .catch( error => {
-      response.status(500).json( { error })
+      res.status(500).json( { error })
     })
-
 })
 
 app.listen(app.get('port'), () => {
