6747c2b4cffa2ec4105d12ed13a0e8d8be821c56
RyanFlachman <ryan@flachman.net>
2019-08-17 20:53:48 -0600

    Add football_quarterbacks table

--- server.js
+++ server.js
@@ -16,11 +16,15 @@ app.get('/api/v1/teams', (req, res) => {
     })
 })
 
-app.get('api/v1/teams/:id', (req, res) => {
+app.get('/api/v1/teams/:id', (req, res) => {
   database('football_teams')
-    .where({id: req.params.id})
-    .then()
-
+    .where({ id: req.params.id})
+    .then( teams => {
+      res.status(200).json(teams)
+    })
+    .catch( error => {
+      res.status(404).json({ error })
+    })
 })
 
 app.listen(app.get('port'), () => {
